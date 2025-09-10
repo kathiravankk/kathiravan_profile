ad381bba2de8e1c4ce8fb4a9ebb99dfffead7bf3
Kathiravan <kathiravan.k@datayaan.com>
2025-09-10 22:27:09 +0530

    gghjkl

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/kathiravan_profile',
+  // base: '/kathiravan_profile',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
