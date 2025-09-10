161c6cb60e28ffe0307e96cd7eae242dd0962b76
Kathiravan <kathiravan.k@datayaan.com>
2025-09-10 22:07:42 +0530

    098765

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 3000,
-  },
-    base: '/kathiravan_profile/' ,
+  base: '/kathiravan_profile',
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 }));
